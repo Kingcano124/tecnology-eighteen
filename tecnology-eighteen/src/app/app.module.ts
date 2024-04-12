88c3fcbb89755c99b9fa7b7b73b4d2ea3d3b1bb0
David Sánchez Cano <davix.sevas@gmail.com>
2024-04-11 22:36:28 -0500

    Se crea el componente footer, adicional se corrige la instalación de angular material se verifica y queda OK.

--- tecnology-eighteen/src/app/app.module.ts
+++ tecnology-eighteen/src/app/app.module.ts
@@ -6,25 +6,30 @@ import { AppComponent } from './app.component';
 import { CardProductsComponent } from './card-products/card-products.component';
 
 
-//**********Material Angular */
+/* Material Angular */
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
-
+import { FooterComponent } from './footer/footer.component';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardProductsComponent,
     IniciarSesionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
